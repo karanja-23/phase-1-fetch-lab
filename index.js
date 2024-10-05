e750089c4f9acef2c4c9e5063cbd424529016d74
Hosea <karanja.mungai23@gmail.com>
2024-10-05 09:10:25 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then (response => response.json())
+  .then (data => renderBooks(data))
   
 }
 
@@ -15,3 +18,7 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+fetch ('https://anapioficeandfire.com/api/books')
+.then (response => response.json())
+.then (data => console.log(data))
